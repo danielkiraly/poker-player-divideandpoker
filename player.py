b2a2347a3b041c62d2689966ad50767f99cfbd07
Eszter Szluka <szluka.eszter@gmail.com>
2019-09-02 10:25:13 +0200

    add json

--- player.py
+++ player.py
@@ -1,8 +1,12 @@
-
+import json
 class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
+        data = json.load(game_state)
+        """players = data["players"]
+        hole_cards = players["hole"]"""
+        print(data)
         return 0
 
     def showdown(self, game_state):
