06ce5d3897173373fe61bfea44016b9a4fc3c44b
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2019-09-02 12:15:46 +0200

    add game algorithm

--- player.py
+++ player.py
@@ -18,3 +18,20 @@ class Player:
         import sys
         print(message, file=sys.stderr)"""
 
+
+    def isPair(self, cards):
+        if cards[0]["rank"] == cards[1]["rank"]:
+            return True
+        else:
+            return False
+
+
+    def isSameColor(self, cards):
+        if cards[0]["suit"] == cards[1]["suit"]:
+            return True
+        else:
+            return False
+
+
+    def isHighCard(self, cards):
+
