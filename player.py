0e8eddf7f46d113dee4c54a4c8e4237a27a9f73b
Daniel Kiraly <danielkiraly@ymail.com>
2019-09-02 11:22:58 +0200

    Test logging

--- player.py
+++ player.py
@@ -6,22 +6,15 @@ class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
-        """data = json.load(game_state)
+        data = json.load(game_state)
         players = data["players"]
-        hole_cards = []
-        for player in players:
-            if player["name"] == "DivideAndPoker":
-                hole_cards = player["hole_cards"]
-        if hole_cards[0]["rank"] == hole_cards[1]["rank"]:
-            return 10
-        else:
-            return 0"""
+        self.log(players)
         return 600
 
     def showdown(self, game_state):
         pass
 
-    def log(message):
+    def log(self, message):
         import sys
         print(message, file=sys.stderr)
 
