095f29187ff3bc59efefeeb77ebff5ed2a8a9c1f
Eszter Szluka <szluka.eszter@gmail.com>
2019-09-02 10:43:33 +0200

    add pairs_check

--- player.py
+++ player.py
@@ -4,10 +4,16 @@ class Player:
 
     def betRequest(self, game_state):
         data = json.load(game_state)
-        """players = data["players"]
-        hole_cards = players["hole"]"""
-        print(data)
-        return 0
+        players = data["players"]
+        hole_cards = []
+        for player in players:
+            if player["name"] == "DivideAndPoker":
+                hole_cards = player["hole_cards"]
+        if hole_cards[0]["rank"] == hole_cards[1]["rank"]:
+            return 10
+        else:
+            return 0
+
 
     def showdown(self, game_state):
         pass
