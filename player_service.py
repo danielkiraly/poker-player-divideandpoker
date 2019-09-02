cc634c599cafd987146a20c4262a25b2fdba58ad
Daniel Kiraly <danielkiraly@ymail.com>
2019-09-02 10:34:26 +0200

    Print json data through service

--- player_service.py
+++ player_service.py
@@ -38,6 +38,8 @@ class PlayerService(BaseHTTPServer.BaseHTTPRequestHandler):
         response = ''
         if action == 'bet_request':
             response = Player().betRequest(game_state)
+            data = json.load(game_state)
+            print(json.dumps(data, indent=4, sort_keys=True))
         elif action == 'showdown':
             Player().showdown(game_state)
         elif action == 'version':
