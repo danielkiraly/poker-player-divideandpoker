0470cf5a6691e615d904daae9a1a92dd49076742
Barnabas Bucsek <bucsekbarnabas@gmail.com>
2019-09-02 10:49:34 +0200

    raise hard

--- player.py
+++ player.py
@@ -5,7 +5,7 @@ class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
-        data = json.load(game_state)
+        """data = json.load(game_state)
         players = data["players"]
         hole_cards = []
         for player in players:
@@ -14,7 +14,8 @@ class Player:
         if hole_cards[0]["rank"] == hole_cards[1]["rank"]:
             return 10
         else:
-            return 0
+            return 0"""
+        return 501
 
     def showdown(self, game_state):
         pass
