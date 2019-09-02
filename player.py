b4ba31e7a340e86103599de186775592645538a7
Daniel Kiraly <danielkiraly@ymail.com>
2019-09-02 13:47:35 +0200

    Checking exact card logging

--- player.py
+++ player.py
@@ -1,11 +1,18 @@
 from __future__ import print_function
-import json
 
 
 class Player:
     VERSION = "1.1"
 
     def betRequest(self, game_state):
+        try:
+            players = game_state["players"]
+            hole_cards = []
+            for player in players:
+                if player['name'] == 'DivideAndPoker':
+                    hole_cards = player['hole_cards']
+            self.log(hole_cards[0]['rank'])
+        except:
         players = game_state["players"]
         hole_cards = []
         for player in players:
